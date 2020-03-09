437c484044cb11687f5d05bceca6babe47644d01
Juan Millord <jmillord@ambiente.gob.do>
2020-03-09 12:37:03 -0400

    hashed the password

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,26 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const { registerValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
+  //VALIDATE THE USER
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  //CHECK IF THE USER IS IN THE DATABASE
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exist");
+
+  // HASH THE PASSWORD
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  //CREATE THE USER
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashPassword
   });
   try {
     const savedUser = await user.save();
